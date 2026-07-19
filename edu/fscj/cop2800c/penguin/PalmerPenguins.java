a60e189bfa64a52ee2d5e7680cea3eb39985a295
latoyasmothers-tolar-bit <lavirgo0318@yahoo.com>
2026-07-19 16:08:34 -0400

    PalmerPenguins.java

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -1,6 +1,6 @@
 // PalmerPenguins.java
-// D. Singletary
-// 2/22/25
+// Latoya Smothers-Tolar
+// July 19, 2026
 // Refactored Palmer Penguins analysis using object-oriented approach
 
 package edu.fscj.cop2800c.penguin;
@@ -8,7 +8,16 @@ package edu.fscj.cop2800c.penguin;
 public class PalmerPenguins {
     public static void main(String[] args) {
         // Create an instance of PenguinAnalyzer
+        PenguinAnalyzer analyzer = new PenguinAnalyzer();
 
+        // Read CSV data
+        analyzer.readPenguinData();
+
+        //Print formatted output
+        analyzer.printPenguinData();
+
+        //Save results to a file
+        analyzer.writePenguinData();
 
     }
 }
